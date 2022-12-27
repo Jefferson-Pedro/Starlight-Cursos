5f7794b7e09d0420f986b13f938c57b853d64650
Jefferson Pedro <jeffersonpedro05@gmail.com>
2022-12-26 21:47:26 -0300

    Update CourseController.java
    -  Criando um método para buscar um registro por ID na API REST no Spring.

--- crud-spring/src/main/java/br/com/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/br/com/crudspring/controller/CourseController.java
@@ -1,18 +1,16 @@
 package br.com.crudspring.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.crudspring.model.Courses;
 import br.com.crudspring.repositories.CourseRepository;
 
@@ -27,6 +25,14 @@ public class CourseController {
 	@GetMapping
 	public List<Courses> list(){
 		return cr.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Courses> findById(@PathVariable Long id) {
+		return cr.findById(id)
+				.map(registro -> ResponseEntity.ok().body(registro))
+				.orElse(ResponseEntity.notFound().build());
+		
 	}
 	
 	@PostMapping
