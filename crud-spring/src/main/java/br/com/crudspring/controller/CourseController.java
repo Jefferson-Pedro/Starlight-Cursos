3dd526f950eb48f4e3f63f71dc0639cee5ba4eae
Jefferson Pedro <jeffersonpedro05@gmail.com>
2022-12-27 20:45:59 -0300

    Criação do método Delete no Spring

--- crud-spring/src/main/java/br/com/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/br/com/crudspring/controller/CourseController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,5 +57,14 @@ public class CourseController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		return cr.findById(id)
+				.map(registroEncontrado -> {
+					cr.deleteById(id);
+					return ResponseEntity.ok("Curso deletado!");
+				})
+				.orElse(ResponseEntity.notFound().build());
+	}
 	
 }
