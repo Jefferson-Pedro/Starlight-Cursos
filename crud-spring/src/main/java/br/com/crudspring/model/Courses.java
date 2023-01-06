36264b485c6f38033bf3d8cd8704c8014bc1c41a
Jefferson Pedro <jeffersonpedro05@gmail.com>
2023-01-06 20:29:33 -0300

    update

--- crud-spring/src/main/java/br/com/crudspring/model/Courses.java
+++ crud-spring/src/main/java/br/com/crudspring/model/Courses.java
@@ -22,5 +22,29 @@ public class Courses {
 	
 	@Column(length = 20, nullable = false)
 	private String category;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
 	
 }
