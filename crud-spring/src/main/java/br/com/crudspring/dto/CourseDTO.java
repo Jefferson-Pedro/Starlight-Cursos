47b4b009f66138d84489442c9ca024c706b84db7
Jefferson Pedro <jeffersonpedro05@gmail.com>
2023-03-02 20:45:36 -0300

    Update CourseDTO.java

--- crud-spring/src/main/java/br/com/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/br/com/crudspring/dto/CourseDTO.java
@@ -1,5 +1,18 @@
 package br.com.crudspring.dto;
 
-public class CourseDTO {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+
+public record CourseDTO(
+		@JsonProperty("_id") Long id, 
+		@NotBlank @NotNull @Size(min = 5, max=100) String name, 
+		@NotNull @Size(max=20) @Pattern(regexp = "back-end|front-end|fullstack") String category, 
+		@NotNull@Size(max=20) @Pattern(regexp = "Ativo|Inativo") String status) {
+	
+	
 }
